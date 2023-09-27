c106bbedbafa6ab0f1a0611f8564170756c201b1
tito-mov <titozinnz@gmail.com>
2023-09-27 20:45:33 -0300

    02 - O que são componentes
    - Como criar uma nova prop;
    
    -Como passar esta prop para inúmeros componentes;
    
    -O que é uma closure e como utilizá-la a seu favor;
    
    -Como instalar novas dependências na sua aplicação;
    
    -Diferença entre export e export default.

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -27,7 +27,7 @@ const Rodape = () => {
        </section>
        <section>
         <p>
-            Desenvolvido por Alura.
+            Desenvolvido por Tito.
         </p>
        </section>
     </footer>)
