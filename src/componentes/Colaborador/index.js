4dd8fee6b3305a86db7c9351538981446dfb7384
tito-mov <titozinnz@gmail.com>
2023-09-28 01:19:46 -0300

    05 - Finalizando o Projeto
    - Como criar um ternário para renderizar de forma opcional;
    
    - Como renderizar condicionais de outras formas;
    
    - Como refatorar um componente sem atrapalhar a execução primária dele;
    
    - A importância de se refatorar um componente em projetos React.

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,19 @@
-import { AiFillCloseCircle } from 'react-icons/ai';
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id);
+    }
+
+    const propsfavorito = {
+        size: 25,
+        onClick: favoritar,
+    }
+
+
+
+
     return (<div className="colaborador">
         <AiFillCloseCircle size={25} className="deletar" onClick={() => aoDeletar(colaborador.id)} />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
@@ -10,6 +22,12 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito
+                    ? <AiFillHeart {...propsfavorito}  color='#ff0000'/>
+                    : <AiOutlineHeart {...propsfavorito} />
+                }
+            </div>
         </div>
     </div>)
 }
